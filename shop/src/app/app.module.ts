7accab250e42b5bec329dc4bd1aa824bc6ab475f
thenguyen123 <the78901234@gmail.com>
2023-04-17 20:14:36 +0700

    add

--- shop/src/app/app.module.ts
+++ shop/src/app/app.module.ts
@@ -5,17 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { DetailComponent } from './detail/detail.component';
+import { ListComponent } from './list/list.component';
+import { CardComponent } from './card/card.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DetailComponent,
+    ListComponent,
+    CardComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
